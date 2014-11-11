a646a09b440647315ae2fcae69626cbe99cbd786
Leo Dintner <leodintner@yahoo.de>
2014-11-11 12:02:13 +0100

    forgot to set correct callback interface

--- src/at/oeldin/motes/MainActivity.java
+++ src/at/oeldin/motes/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends ActionBarActivity implements OnItemSelectedLis
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			// Apply the adapter to the spinner
 			spinner.setAdapter(adapter);
+			spinner.setOnItemSelectedListener(this);
 	        }
 	    }
 
